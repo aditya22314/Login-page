2264d413532f8409046823a8b18c59486769f628
aditya2234 <tejusadianu500@gmail.com>
2023-10-25 09:16:38 +0530

    Major Changes

--- Login/src/Login1.jsx
+++ Login/src/Login1.jsx
@@ -1,8 +1,41 @@
 import React from 'react'  
+import {useState} from 'react'
 import { faUser, faLock } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; 
+import * as yup from 'yup';  
 
- export const Login1 = () => {
+const validationSchema = yup.object().shape({
+  username: yup.string().min(5, '*Username must be at least 5 characters').required('Username is required'),
+  password: yup.string().min(6, '*Password must be at least 6 characters').required('Password is required'),
+});
+
+ export const Login1 = () => {  
+
+  const [formData, setFormData] = useState({
+    username: '',
+    password: '',
+  });  
+
+  const [errors, setErrors] = useState({}); 
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    }); 
+   
+    
+    validationSchema
+    .validateAt(name, formData)
+    .then(() => {
+      setErrors({ ...errors, [name]: '' });
+    })
+    .catch((error) => {
+      setErrors({ ...errors, [name]: error.message });
+    });
+};
+
 
   return (
     <>  
@@ -19,16 +52,25 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
                 <div className={' flex  justify-center gap-10 '}> 
                 <FontAwesomeIcon icon={faUser} style={{ color: '#2c3068' }} size="lg" />
                  <label >Username</label>
-                 <input className={''}type="text" name="" id="" /> 
+                 <input placeholder='Enter username' className={' relative   rounded-md'}type="text" name="username" id="username"  value={formData.username}
+                onChange={handleChange} /> 
+                  {errors.username && <div className="text-red-500 absolute  mt-[30px]  font-extrabold text-xs right-[628px]">{errors.username}</div>}
                  </div>  
+                
                  <div className={'flex  justify-center gap-10'}> 
                  <FontAwesomeIcon icon={faLock} style={{ color: '#2c3068' }} size="lg" />
                  <label >Password</label>
-                 <input  type="password" name="" id="" /> 
-                 </div>  
+                 <input placeholder='Enter password' className={' relative rounded-md'}type="password"  name="password"
+                id="password"
+                value={formData.password}
+                onChange={handleChange} />  
+                {errors.password && <div className="text-red-500 absolute  mt-[30px] text-xs font-extrabold right-[628px] ">{errors.password}</div>}
+                 </div>   
+                 
                  <div className={'text-center  mb-[40px]'}>
                  <a  
-                 >Submit</a> </div> 
+                 >Submit</a> </div>  
+                 
                  {/* <div className={'flex  justify-center'}><button className='  bg-mantis px-2 py-2  rounded-xl hover:bg-white mb-14'>Submit</button></div> */}
                  </div>   
                  <div className='flex  mb-3'>   
